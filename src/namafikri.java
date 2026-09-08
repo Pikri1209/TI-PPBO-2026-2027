b78002161761d95b67614627d8ddd26012c2e62b
Pikri Ramadhan Maha <panwaslihsinehu@gmail.com>
2026-09-08 10:26:51 +0700

    variabel

--- src/namafikri.java
+++ src/namafikri.java
@@ -1,6 +1,10 @@
 public class namafikri {
     public static void main(String[] args) {
+        // Program menampilkan biodata sederhana
     System.out.println("nama: fikri");
+
+    /* Baris di bawah ini
+           mencetak NIM mahasiswa */
     System.out.println("Nim: 2025573010021");
     }
 }
