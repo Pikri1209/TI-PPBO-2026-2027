e9810a0be22713108758d76bfa44cb975911a84f
Pikri1209 <panwaslihsinehu@gmail.com>
2026-09-08 12:34:13 +0700

    Delete src directory

--- src/nama.java
+++
@@ -1,8 +0,0 @@
-public class nama {
-    public static void main(String[] args) {
-        System.out.print("Nama: ");
-        System.out.print("Fikri");
-        System.out.println();
-        System.out.println("Ini baris baru");
-    }
-}
